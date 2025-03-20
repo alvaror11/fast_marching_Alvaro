fee283e82a8d969173fb6aac7f2e634aef604755
alvaror11 <alvaroramos.tomas@gmail.com>
2025-03-20 16:55:09 +0700

    Añadir otra vez el printeo a archivos para visualización

--- functions/FMM2.c
+++ functions/FMM2.c
@@ -77,14 +77,65 @@ void FMM2_2D(double* matriz, int* size_map, double distance_threshold, double sa
     //Create velocities map
     
     double* obstacle_distance_map = velocities_map(matriz, size_map, distance_threshold, safety_margin);
-        
+    // Save velocities map
+    FILE *output_file1 = fopen("./Archivos/velocities_map.txt", "w");
+     if (output_file1 == NULL) {
+         perror("Error al abrir el archivo de salida");
+         return;
+     }
+     for (int i = 0; i < filas; i++) {
+         for (int j = 0; j < columnas; j++) {
+             fprintf(output_file1, "%.2f ", obstacle_distance_map[j + i * columnas]);
+         }
+         fprintf(output_file1, "\n");
+     }
+     fclose(output_file1); 
+
     //Allocate memory for output map
     double* output_T = (double *)malloc(filas * columnas * sizeof(double));
     output_T = main_msfm(obstacle_distance_map, objective_points, output_T, size_map, size_objective);
+    // Save times map
+    FILE *output_file2 = fopen("./Archivos/times_map.txt", "w");
+     if (output_file2 == NULL) {
+         perror("Error al abrir el archivo de salida");
+         return;
+     }
+ 
+     // Escribir los resultados del mapa de distancias en el archivo de salida
+     for (int i = 0; i < filas; i++) {
+         for (int j = 0; j < columnas; j++) {
+             fprintf(output_file2, "%.2f ", output_T[j + i * columnas]);
+         }
+         fprintf(output_file2, "\n");
+     }
+     fclose(output_file2);
 
     // Crear el gradiente para el mapa de tiempos:
     double* gradient_matrix = (double*)malloc(2 * filas * columnas * sizeof(double));
     compute_gradient_2d_discrete(output_T, gradient_matrix, size_map);
+    // Save gradients
+    FILE *gradient_x_file = fopen("./Archivos/gradient_x.txt", "w");
+    FILE *gradient_y_file = fopen("./Archivos/gradient_y.txt", "w");
+ 
+     if (gradient_x_file == NULL || gradient_y_file == NULL) {
+         perror("Error al abrir los archivos de gradiente");
+         return;
+     }
+ 
+     // Write gradient components in matrix format
+     for (int i = 0; i < filas; i++) {
+         for (int j = 0; j < columnas; j++) {
+             // Write x component
+             fprintf(gradient_x_file, "%.4f ", gradient_matrix[i*columnas + j]);
+             // Write y component
+             fprintf(gradient_y_file, "%.4f ", gradient_matrix[i*columnas + j + filas*columnas]);
+         }
+         fprintf(gradient_x_file, "\n");
+         fprintf(gradient_y_file, "\n");
+     }
+ 
+     fclose(gradient_x_file);
+     fclose(gradient_y_file);
 
     // Empezamos a usar el descenso del gradiente para buscar el camino
     bool finished = false;      //mientras no se llegue al punto final es false
@@ -116,7 +167,20 @@ void FMM2_2D(double* matriz, int* size_map, double distance_threshold, double sa
             traj->points[traj->size - 1].y = objective_points[1];
         }
     }
-
+    FILE *traj_file = fopen("./Archivos/trajectory.txt", "w");
+     if (traj_file == NULL) {
+         perror("Error al abrir el archivo de trayectoria");
+         return;
+     }
+ 
+     for (int i = 0; i < traj->size; i++) {
+         double traj_x = traj->points[i].x;
+         double traj_y = traj->points[i].y;
+         // Save to file (x y format)
+         fprintf(traj_file, "%.2f %.2f\n", traj_x, traj_y);
+     }
+ 
+     fclose(traj_file);
     // Liberar memoria    
     free(output_T);
     free(matriz);
