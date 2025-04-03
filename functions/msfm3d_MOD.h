583e9250a218b9d86681fbed8d57be4150debb7e
alvaror11 <alvaroramos.tomas@gmail.com>
2025-04-03 23:08:32 +0700

    no cambios, arreglando git timeline

--- functions/msfm3d_MOD.h
+++ functions/msfm3d_MOD.h
@@ -1,9 +1,9 @@
 #ifndef MSFM2D_MOD_H
 #define MSFM2D_MOD_H
 
-    float* main_msfm3D(float* F, float* source_points, float* T, int* size_map, int* size_target,
-                        float* start_points);
-    void compute_gradient_3d_discrete(float* input_matrix, float* gradient_matrix, int* size_map);
-    float* velocities_map3D(float* matriz, int* size_map, int distance_threshold);
+    double* main_msfm3D(double* F, double* source_points, double* T, int* size_map, int* size_target,
+                        double* start_points);
+    void compute_gradient_3d_discrete(double* input_matrix, double* gradient_matrix, int* size_map);
+    double* velocities_map3D(double* matriz, int* size_map, int distance_threshold);
 
 #endif 
--- functions/planner.h
+++ functions/planner.h
@@ -3,18 +3,18 @@
 
 #include <stdbool.h>
 
-void planners_2D(float* matriz, 
+void planners_2D(double* matriz, 
                  int* size_map, 
-                 float* objective_points, 
+                 double* objective_points, 
                  int size_objective[2], 
-                 float* start_points, int size_start[2], 
+                 double* start_points, int size_start[2], 
                  int planner_type, int escalado_vectores);
 
-void planners_3D(float* matriz, 
+void planners_3D(double* matriz, 
                   int* size_map, 
-                  float* objective_points, 
+                  double* objective_points, 
                   int size_objective[2], 
-                  float* start_points, int size_start[2], 
+                  double* start_points, int size_start[2], 
                   int planner_type, int escalado_vectores);
 
 #endif // PLANNER_H
